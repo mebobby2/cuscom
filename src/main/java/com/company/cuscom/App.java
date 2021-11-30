67d6edccf0af45534c65a499e9e79f8b3a0522a6
Bobby Lei <bobl53@gmail.com>
2021-11-30 22:31:46 +1300

    Revert "Use Spring DSL to write Camel routes"
    This reverts commit 13e9433e818572d528d2779eb5041179d79c947f.

--- src/main/java/com/company/cuscom/App.java
+++ src/main/java/com/company/cuscom/App.java
@@ -11,5 +11,11 @@ public class App extends RouteBuilder {
 
   @Override
   public void configure() throws Exception {
+    from("file:inbound")
+      .choice()
+      .when(header("NeedsApproval").isEqualTo(true))
+      .to("jms:queue:APPROVAL")
+      .otherwise()
+      .to("seda:transform");
   }
 }
